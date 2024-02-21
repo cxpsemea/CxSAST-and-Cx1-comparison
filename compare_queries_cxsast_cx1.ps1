3a8f7c761743ab10058f4f5fab9db58ef077a949
michael kubiaczyk <michael.kubiaczyk@checkmarx.com>
2024-02-21 15:50:24 +0100

    more

--- compare_queries_cxsast_cx1.ps1
+++ compare_queries_cxsast_cx1.ps1
@@ -41,10 +41,6 @@ function req($uri, $method, $token, $errorMessage, $body){
         }
         return $resp
     } catch {
-        log $_
-        $value = $_.Exception.Response.StatusCode.value__
-        log "StatusCode: ${value}" 
-        log "Request body was: $($body | ConvertTo-Json)"
         throw $errorMessage
     }
 }
@@ -600,11 +596,13 @@ foreach ( $row in $SASTQueriesByID.GetEnumerator() ) {
         $astID = "$($row.Value.AstID)"
 
         if ( $Cx1QueriesByID.ContainsKey($astID) ) {
-            if ( $Cx1QueriesByID[$astID].Severity -eq "" -and ($CorpOnly -and $row.Value.CorpID -ne "") ) {
+            if ( $Cx1QueriesByID[$astID].Severity -eq "" -and (-Not $CorpOnly -or $row.Value.CorpID -ne "") ) {
                 try {
                     $q = getCx1QueryDetails $Cx1URL $Cx1Token "Cx" $Cx1QueriesByID[$astID].Path "Failed to get details for Cx query $($Cx1QueriesByID[$astID].Path)"
                     $Cx1QueriesByID[$astID].Severity = sevstr $q.Severity
-                } catch {}
+                } catch {
+                    log $_.Exception
+                }
             }
             if ( $row.Value.Mapped ) {
 				$Cx1QueriesByID[$astID].Mapped = $true
